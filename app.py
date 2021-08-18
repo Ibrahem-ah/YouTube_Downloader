d0e656ebc51513658ef97035254bfaa25e1caa73
Ibrahem-ah <i-a-h-1911@hotmail.com>
2021-08-18 21:43:25 +0300

    Version 1.0

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from random import randint
-from flask import Flask, request, render_template, redirect, send_file, send_from_directory
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    send_file,
+    send_from_directory,
+)
 import youtube_dl
 import os
 import random
@@ -17,27 +24,33 @@ def download():
     if request.method == "POST":
         url = request.form["URL"]
         filename = downloadURL(url)
-        print('\n\n', filename, '\n\n')
-       
+        print("\n\n", filename, "\n\n")
+
         try:
-            return send_from_directory('./videos', f'{filename}.webm', as_attachment=True)
+            return send_from_directory(
+                "./videos", f"{filename}.webm", as_attachment=True
+            )
         except:
-            return send_from_directory('./videos', f'{filename}.mkv', as_attachment=True)
-        
-    return render_template('test.html')
+            return send_from_directory(
+                "./videos", f"{filename}.mkv", as_attachment=True
+            )
+
+    return render_template("test.html")
 
 
 def downloadURL(url):
     rannum = randint(0, 100000)
     ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'moplaylist': True,
-        'outtmpl': './videos/' f'{rannum}'
+        "format": "bestvideo+bestaudio/best",
+        "moplaylist": True,
+        "outtmpl": f"./videos/{rannum}",
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-        return rannum
-
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except:
+        print("Could not download the video in course: {}".format(url))
+    return rannum
 
 if __name__ == "__main__":
     app.run(debug=True)
